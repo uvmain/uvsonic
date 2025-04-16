763a1035c6f71203ac9b39c91e9547c89b4b34aa
Ian Baron <ian.baron@manchester.ac.uk>
2025-04-16 14:23:24 +0100

    insert rows into DB

--- backend/files/import.go
+++ backend/files/import.go
@@ -6,29 +6,38 @@ import (
 	"github.com/uvmain/uvsonic/db"
 	"github.com/uvmain/uvsonic/ff"
 	"github.com/uvmain/uvsonic/logic"
+	"github.com/uvmain/uvsonic/types"
 )
 
+var metadataChannel = make(chan types.TrackMetadata, 100)
+
 func Init() {
+	go startMetadataWorker()
 	importFiles()
 }
 
 func importFiles() {
 	filePaths, _ := logic.GetDirContents(logic.AudioFilesDirectory, logic.AudioFileTypes)
 	for _, filePath := range filePaths {
-		go importFile(filePath)
+		go processFile(filePath)
 	}
 }
 
-func importFile(filepath string) {
+func processFile(filepath string) {
 	metadata, err := ff.GetTags(filepath)
 	if err != nil {
 		log.Printf("Failed to get tags for file %s: %s", filepath, err)
 		return
 	}
 
-	err = db.InsertTrackMetadata(metadata)
-	if err != nil {
-		log.Printf("Failed to get tags for file: %s", err)
-		return
+	metadataChannel <- metadata
+}
+
+func startMetadataWorker() {
+	for metadata := range metadataChannel {
+		err := db.InsertTrackMetadata(metadata)
+		if err != nil {
+			log.Printf("Failed to insert metadata for file %s: %s", metadata.Filename, err)
+		}
 	}
 }
